52774de4337b6d8d06c32f1d3579fe2122c0af42
Alexandru Munteanu <alexandru.munt@gmail.com>
2024-11-04 13:40:38 +0200

    Adding Login and Signup pages

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,31 +1,77 @@
+import { useEffect, useState } from "react";
+
+import { Auth } from "aws-amplify";
+import { useNavigate } from "react-router-dom";
+
 import { Nav, Navbar } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 
 import "./App.css";
 
 import Routes from "./Routes";
+import { onError } from "./lib/errorLib";
+import { AppContext, AppContextType } from "./lib/contextLib";
 
 function App() {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const nav = useNavigate();
+
+  async function handleLogout() {
+    await Auth.signOut();
+    userHasAuthenticated(false);
+    nav("/login");
+  }
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    } catch (error) {
+      if (error !== "No current user") {
+        onError(error);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
   return (
-    <div className="App container py-3">
-      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
-        <LinkContainer to="/">
-          <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Nav activeKey={window.location.pathname}>
-            <LinkContainer to="/signup">
-              <Nav.Link>Signup</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Nav.Link>Login</Nav.Link>
-            </LinkContainer>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes />
-    </div>
+    !isAuthenticating && (
+      <div className="App container py-3">
+        <Navbar collapseOnSelect bg="light" expand="md" className="mb-3 px-3">
+          <LinkContainer to="/">
+            <Navbar.Brand className="fw-bold text-muted">Scratch</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle />
+          <Navbar.Collapse className="justify-content-end">
+            <Nav activeKey={window.location.pathname}>
+              {isAuthenticated ? (
+                <Nav.Link onClick={handleLogout}>Logout</Nav.Link>
+              ) : (
+                <>
+                  <LinkContainer to="/signup">
+                    <Nav.Link>Signup</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to="/login">
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                </>
+              )}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <AppContext.Provider
+          value={{ isAuthenticated, userHasAuthenticated } as AppContextType}
+        >
+          <Routes />
+        </AppContext.Provider>
+      </div>
+    )
   );
 }
 
--- packages/frontend/src/Routes.tsx
+++ packages/frontend/src/Routes.tsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from "react-router-dom";
+
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login.tsx";
+import Signup from "./containers/Signup.tsx";
 
 export default function Links() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
